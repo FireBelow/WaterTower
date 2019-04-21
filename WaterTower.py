a5286724ee7c3de3d5cf810f7700085dc51b076c
FireBelow <FireBelow@users.noreply.github.com>
2019-04-21 00:55:30 -0400

    Start of functions and main code

--- WaterTower.py
+++ WaterTower.py
@@ -15,9 +15,6 @@ import time
 # from watertowerfunctions import write_file  #, get_weather, IFTTTmsg, check_web_response, weather_date_only    # read_scale, calculate,
 
 # Turn on/off each valve based on CSV schedule
-# Stop pump if Supply is empty
-# Stop pump if Tower is full
-# Turn on Pump if Tower Empty
 # Check Tower every 2 seconds when valve is on
 # If rain is predicted delay watering to see if rain falls (and how much)
 # If rain is predicted and storage is full, consider emptying rain barrels depending on volume forecasted
@@ -58,7 +55,7 @@ Pin_Sensor_TowerEmpty = 27
 Pin_Sensor_StorageFull = 4
 Pin_Sensor_StorageEmpty = 14
 Pin_Sensor_WaterFlow = 15
-Pin_Sensors = [Pin_Sensor_TowerFull, Pin_Sensor_TowerEmpty, Pin_Sensor_StorageFull]#, Pin_Sensor_StorageEmpty]
+Pin_Sensors = [Pin_Sensor_TowerFull, Pin_Sensor_TowerEmpty, Pin_Sensor_StorageFull, Pin_Sensor_StorageEmpty]
 Pin_Valve_Bed1Alley = 23
 Pin_Valve_Bed2 = 24
 Pin_Valve_Bed3 = 25
@@ -77,27 +74,92 @@ Pin_Pump = 18
 # Pin Setup:
 GPIO.setmode(GPIO.BCM)  # Broadcom/GPIO pin-numbering scheme not Board Pins
 GPIO.setup(Pin_Sensors, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(Pin_Sensor_WaterFlow, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(Pin_Sensor_WaterFlow, GPIO.IN, pull_up_down=GPIO.PUD_UP)     # Pull up input because we read sensor when it is low
 GPIO.setup(Pin_Valves, GPIO.OUT)
-GPIO.output(Pin_Valves, GPIO.LOW)
+GPIO.output(Pin_Valves, GPIO.HIGH)  # HIGH is OFF ****
 GPIO.setup(Pin_LEDs, GPIO.OUT)
 GPIO.output(Pin_LEDs, GPIO.LOW)
 GPIO.setup(Pin_Pump, GPIO.OUT)
 GPIO.output(Pin_Pump, GPIO.LOW)
 print("Setup Done")
 
+WET = 0
+DRY = 1
+WET_STORAGEFULL = 1
+DRY_STORAGEFULL = 0
+RUNNING = 1
+STOPPED = 0
+
+
 # Initial state for Outputs:
 # GPIO.output(Pin_Valves, GPIO.HIGH)        # Relay board in use uses HIGH as off
-GPIO.output(Pin_LEDs, GPIO.LOW)
-time.sleep(5)
-GPIO.output(Pin_LEDs, GPIO.HIGH)
-print("Set Hi")
-try:
-    time.sleep(5)
-    GPIO.output(Pin_LEDs, GPIO.LOW)
-    print("Set Low")
-    time.sleep(5)
+# GPIO.output(Pin_LEDs, GPIO.LOW)
+# time.sleep(2)
 
+def Pump_TurnOn(Pump_Pin):
+    # Is there water in Storage
+    print("Storage: {}".format(Sensor_Check(Pin_Sensor_StorageEmpty)))
+    if Sensor_Check(Pin_Sensor_StorageEmpty) is DRY:
+        print("*** No Water ***")
+        return 0
+    else:
+        if Pump_Pin is STOPPED:
+            GPIO.output(Pump_Pin, GPIO.HIGH)
+        else:
+            return 0
+    # Is the Pump Already On, If so check how long it has been on
+
+
+def Pump_TurnOff(Pump_Pin):
+    # Stop pump if Supply is empty
+    # Stop pump if Tower is full
+    GPIO.output(Pump_Pin, GPIO.LOW)
+
+
+def Valve_Open(Pin):
+    GPIO.output(valve, GPIO.LOW)
+
+
+def Valve_Close(Pin):
+    GPIO.output(valve, GPIO.HIGH)
+
+
+def LED_TurnOn(LED_Pin):
+    GPIO.output(LED_Pin, GPIO.HIGH)
+
+
+def LED_TurnOff(LED_Pin):
+    GPIO.output(LED_Pin, GPIO.LOW)
+
+
+def Sensor_Check(Sensor_Pin):
+    return GPIO.input(Sensor_Pin)
+
+try:
+    print("System Test")
+    for LED in Pin_LEDs:
+        LED_TurnOn(LED)
+        # print("Set Hi")
+        time.sleep(.5)
+        LED_TurnOff(LED)
+        # print("Set Low")
+        time.sleep(.5)
+    for valve in Pin_Valves:
+        Valve_Open(valve)
+        # print("Set Hi")
+        time.sleep(4)
+        Valve_Close(valve)
+        # print("Set Low")
+        time.sleep(4)
+    for sensor in Pin_Sensors:
+        print("Sensor Value: {}".format(Sensor_Check(sensor)))
+    Pump_TurnOn(Pin_Pump)
+    time.sleep(2)
+    Pump_TurnOff(Pin_Pump)
+
+# Turn on Pump if Tower Empty
+    if Sensor_Check(Pin_Sensor_TowerEmpty) is DRY:
+        Pump_TurnOn(Pin_Pump)
 
 except:
     # IFTTTmsg("WeightMonitor Exception")
